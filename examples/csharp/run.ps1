8db08a9197f0f75676f34a011ea1968c204ff540
Rui Lopes <rgl@ruilopes.com>
2017-08-08 09:53:41 +0100

    show built image size and history

--- examples/csharp/run.ps1
+++ examples/csharp/run.ps1
@@ -1,7 +1,9 @@
 cd info
 
-Write-Output 'building the container...'
+Write-Output 'building the image...'
 docker build -t csharp-info .
+docker image ls csharp-info
+docker history csharp-info
 
 Write-Output 'running the container in foreground...'
 docker run --rm csharp-info
