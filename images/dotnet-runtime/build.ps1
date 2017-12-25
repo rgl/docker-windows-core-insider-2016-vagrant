e7a07c47cdcbf19a1bf0d29ed83134bfd0711114
Rui Lopes <rgl@ruilopes.com>
2017-12-25 11:54:13 +0000

    upgrade to .net core 2.0.4

--- images/dotnet-runtime/build.ps1
+++ images/dotnet-runtime/build.ps1
@@ -1,5 +1,5 @@
 Write-Output 'building the dotnet-runtime image...'
-$tag = 'dotnet-runtime:2.0.3'
+$tag = 'dotnet-runtime:2.0.4'
 time {docker build -t $tag .}
 docker image ls $tag
 docker history $tag
