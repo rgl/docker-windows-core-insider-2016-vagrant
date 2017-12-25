4ad39a9c683cca7a5fb1d77a3878938ffb6bbfb5
Rui Lopes <rgl@ruilopes.com>
2017-12-25 11:48:45 +0000

    upgrade to honolulu 1711

--- provision-project-honolulu.ps1
+++ provision-project-honolulu.ps1
@@ -2,8 +2,8 @@
 # see https://docs.microsoft.com/en-us/windows-server/manage/honolulu/deployment-guide
 
 # download and install.
-$archiveUrl = 'http://download.microsoft.com/download/E/8/A/E8A26016-25A4-49EE-8200-E4BCBF292C4A/HonoluluTechnicalPreview1709-20016.msi'
-$archiveHash = 'c17364d3064890f02577d66b5b5819dd038754f3d8c827654148e3b2a907461c'
+$archiveUrl = 'http://download.microsoft.com/download/E/8/A/E8A26016-25A4-49EE-8200-E4BCBF292C4A/HonoluluTechnicalPreview1711-15002.msi'
+$archiveHash = '5bee7f16b53c857c06cba9d874f8fe70fdd7f5d7f1139dda4727c2419172a9e9'
 $archiveName = Split-Path -Leaf $archiveUrl
 $archivePath = "$env:TEMP\$archiveName"
 Invoke-WebRequest $archiveUrl -UseBasicParsing -OutFile $archivePath
