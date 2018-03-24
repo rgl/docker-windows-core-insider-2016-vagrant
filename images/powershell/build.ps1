37c17a8efff0e94c3ca376fc26e54d04f27ad34c
Rui Lopes <rgl@ruilopes.com>
2018-03-24 10:03:03 +0000

    upgrade to powershell 6.0.2

--- images/powershell/build.ps1
+++ images/powershell/build.ps1
@@ -1,9 +1,9 @@
 if (!(Test-Path tmp-PowerShell/pwsh.exe)) {
-    $url = 'https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-rc.2/PowerShell-6.0.0-rc.2-win-x64.zip'
-    $sha256 = 'd225b274923e554e14e4b64ea63e470f44acf11b050c8c12afdfd7e54677c443'
-    $filename = Split-Path -Leaf $url
+    $url = 'https://github.com/PowerShell/PowerShell/releases/download/v6.0.2/PowerShell-6.0.2-win-x64.zip'
+    $sha256 = '8cb153e540ed9d9a7fe00cb3d1fe94a0ed089b574fd02e816ab2bb066f4c4f89'
+    $filename = "$PWD\$(Split-Path -Leaf $url)"
     Write-Host ('Downloading Powershell from {0}...' -f $url)
-    Invoke-WebRequest -Uri $url -OutFile $filename
+    (New-Object System.Net.WebClient).DownloadFile($url, $filename)
     Write-Host ('Verifying sha256 ({0})...' -f $sha256)
     if ((Get-FileHash $filename -Algorithm sha256).Hash -ne $sha256) {
         Write-Host 'FAILED!'
@@ -16,7 +16,7 @@ if (!(Test-Path tmp-PowerShell/pwsh.exe)) {
 }
 
 Write-Output 'building the powershell image...'
-$tag = 'powershell:6.0.0-rc.2'
+$tag = 'powershell:6.0.2'
 time {docker build -t $tag .}
 docker image ls $tag
 docker history $tag
