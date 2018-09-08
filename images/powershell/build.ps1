4dcd5dcaa3442ceb80f1cefeb23f63a95fad990d
Rui Lopes <rgl@ruilopes.com>
2018-09-08 15:37:41 +0100

    simplify the powershell image creation

--- images/powershell/build.ps1
+++ images/powershell/build.ps1
@@ -1,23 +1,21 @@
-if (!(Test-Path tmp-PowerShell/pwsh.exe)) {
-    $url = 'https://github.com/PowerShell/PowerShell/releases/download/v6.0.4/PowerShell-6.0.4-win-x64.zip'
-    $sha256 = '0b04b63d2b63d4631cf5bd6e531f26b60f3cc1b1db41c8b5360f14776e66f797'
-    $filename = "$PWD\$(Split-Path -Leaf $url)"
-    Write-Host ('Downloading Powershell from {0}...' -f $url)
+$version = '6.0.4'
+$sha256 = '0b04b63d2b63d4631cf5bd6e531f26b60f3cc1b1db41c8b5360f14776e66f797'
+$url = "https://github.com/PowerShell/PowerShell/releases/download/v$version/PowerShell-$version-win-x64.zip"
+$filename = "$PWD\$(Split-Path -Leaf $url)"
+
+if (!(Test-Path $filename)) {
+    Write-Host "Downloading PowerShell from $url..."
     (New-Object System.Net.WebClient).DownloadFile($url, $filename)
-    Write-Host ('Verifying sha256 ({0})...' -f $sha256)
-    if ((Get-FileHash $filename -Algorithm sha256).Hash -ne $sha256) {
-        Write-Host 'FAILED!'
-        Exit 1
-    }
-    Write-Host 'Expanding...'
-    Expand-Archive $filename tmp-PowerShell.tmp
-    Remove-Item $filename
-    Move-Item tmp-PowerShell.tmp tmp-PowerShell
+}
+Write-Host "Verifying sha256 ($sha256)..."
+if ((Get-FileHash $filename -Algorithm sha256).Hash -ne $sha256) {
+    Write-Host 'FAILED! Please remove the file and try again...'
+    Exit 1
 }
 
 Write-Output 'building the powershell image...'
-$tag = 'powershell:6.0.4'
-time {docker build -t $tag .}
+$tag = "powershell:$version"
+time {docker build -t $tag --build-arg POWERSHELL_VERSION=$version .}
 docker image ls $tag
 docker history $tag
 Pop-Location
